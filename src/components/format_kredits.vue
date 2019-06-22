a3cfa36ece4bbe74faad1311240fc582cf778c46
samvicky26 <billavignesh26@gmail.com>
2019-06-22 08:57:01 +0530

    Initial commit

---
+++ src/components/format_kredits.vue
@@ -0,0 +1,33 @@
+<template>
+<span>
+    {{ value }} XKR
+</span>
+</template>
+
+<script>
+export default {
+    name: "FormatKredits",
+    props: {
+        amount: {
+            required: true
+        },
+        round: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    },
+    computed: {
+        value () {
+            let value = this.amount / 1e8
+            if(this.round)
+                value = value.toFixed(3)
+            return value.toLocaleString()
+        }
+
+    }
+}
+</script>
+
+<style>
+</style>
